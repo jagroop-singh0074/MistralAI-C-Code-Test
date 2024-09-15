da93db0cd2758af3578ee8630bb94c5819bf2391
Jagroop Singh <131522392+jagroop-singh0074@users.noreply.github.com>
2024-09-16 06:56:03 +1200

    Create Solution 30.cs

---
+++ Solution 30.cs
@@ -0,0 +1,47 @@
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.Length;
+        int n = nums2.Length;
+        if (m > n) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            int tempInt = m;
+            m = n;
+            n = tempInt;
+        }
+        int left = 0;
+        int right = m;
+        while (left <= right) {
+            int i = (left + right) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (i < m && nums2[j - 1] > nums1[i]) {
+                left = i + 1;
+            } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+                right = i - 1;
+            } else {
+                int maxLeft;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.Max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+                int minRight;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.Min(nums1[i], nums2[j]);
+                }
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0;
+    }
+}
